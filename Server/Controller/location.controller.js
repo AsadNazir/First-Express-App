aab7900f0a8dfd6afee355d8cd6666b5c25bc96d
Asad Nazir <Asad Nazir>
2023-08-21 23:23:08 +0500

    App Completed

--- Server/Controller/location.controller.js
+++ Server/Controller/location.controller.js
@@ -4,7 +4,7 @@ const location = require('../Model/location.model');
 const getAllLocations = async (req, res) => {
     try {
         const categories = await location.getAllLocations();
-        res.status(200).json({ Error: false, Message: "Success", Categories: categories });
+        res.status(200).json({ Error: false, Message: "Success", Location   : categories });
     } catch (err) {
         res.status(500).json({ Error: true, Message: err });
     }
@@ -13,7 +13,7 @@ const getAllLocations = async (req, res) => {
 const getLocationById = async (req, res) => {
     try {
         const loc = await location.getLocationById(req.params.id);
-        res.status(200).json({ Error: false, Message: "Success", Category: loc });
+        res.status(200).json({ Error: false, Message: "Success", Location: loc });
     } catch (err) {
         res.status(500).json({ Error: true, Message: err });
     }
